1a3c304e5bd5db2253044595ba938235fd31142c
morganlevetti <morgan.levetti@novanea.fr>
2022-09-22 15:32:45 +0200

    add interface

--- interface.py
+++ interface.py
@@ -14,22 +14,46 @@ class Interface:
         print('2: Voir ExoCombinaison')
         print('3: Exit')
         choix = input()
+
         if choix == "1":
             print('\n Explore :\n')
             print('Vous aller à la porte de votre Bunker...')
             print('Une voix robotique vous dit...')
             print('"Porte 23A closed !"')
-            print('... Résoudre l"énigme :')
-            print('1... OU 3 ?')
+            print('Résoudre l"énigme :')
+            print('1 OU 3 ?')
             explorechoix = input()
             if explorechoix == "1":
                 print('ERROR 404')
-                print('1... OU 3 ?')
+                print('1 OU 3 ?')
                 explorechoix = input()
                 if explorechoix == "3":
-                    print('SUCCES. porte ouverte')
+                    print('SUCCES. porte ouverte\n')
+                print(" \n ▒█▀█▀█▒█▀█▒▒█▀█▒▄███▄▒"
+                "░█▀█▀█░█▀██░█▀█░█▄█▄█░"
+                "░█▀█▀█░█▀████▀█░█▄█▄█░\n"
+                )
+                print("Vous sortez dehors !")
+                print(" \n ▒█▀█▀█▒█▀█▒▒█▀█▒▄███▄▒"
+                "░█▀█▀█░█▀██░█▀█░█▄█▄█░"
+                "░█▀█▀█░█▀████▀█░█▄█▄█░\n"
+                )
+
+
             elif explorechoix == "3":
-                print('SUCCES. porte ouverte')
+                print('SUCCES. porte ouverte\n')
+                print(" \n ▒█▀█▀█▒█▀█▒▒█▀█▒▄███▄▒"
+                "░█▀█▀█░█▀██░█▀█░█▄█▄█░"
+                "░█▀█▀█░█▀████▀█░█▄█▄█░\n"
+                )
+                print("Vous sortez dehors !")
+                print(" \n ▒█▀█▀█▒█▀█▒▒█▀█▒▄███▄▒"
+                "░█▀█▀█░█▀██░█▀█░█▄█▄█░"
+                "░█▀█▀█░█▀████▀█░█▄█▄█░\n"
+                )
+
+
+
         if choix == "2":
             print('\n ExoCombinaison : \n')
             print('Exo Armor INFO : Statut Correct. Shield =', Armor.shield,'%', 'STH2O =', Armor.sth2o)
